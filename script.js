d626fc5ab2d5609f88ae75770ea93ca93f6a440f
hawk <hm29lind@siena.edu>
2024-02-27 14:09:17 -0500

    fixed page 5

--- script.js
+++ script.js
@@ -1,26 +1,22 @@
-
 "use strict";
 const sub = document.getElementById("submitBtn");
 const textBox = document.getElementById("textBox");
 const answer = "echo";
+console.log("Hello")
 textBox.addEventListener("keyup",event =>{
    
     if(textBox.value === answer)
     {
         document.getElementById("submitBtn").style.background = "green";
+        let form = document.getElementById("echoGame");
+        form.action = "page5.html";
         
     }
     else{
         document.getElementById("submitBtn").style.background = "red";
-    }})
+}})
 
+    
 
-document.getElementById("pathChoice").addEventListener("change",function(){
-    const selectedOption = this.value;
-    const colors = {
-        "strength":"blue",
-        "wisdom": "green",
-        "stealth":"purple"
-    };
-    document.body.style.background = colors[selectedOption] ||"white";
-    });
+    
+    
